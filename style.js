5cb7bfa8ca534430ffb1bab89aabcc2154d8046c
hùng phan <hungphan@Mac-mini-cua-long.local>
2022-07-22 00:04:48 +0700

    nearly done

--- style.js
+++ style.js
@@ -9,8 +9,23 @@ scrollContainer.addEventListener("wheel", (evt) => {
     else {
         scrollContainer.scrollLeft -= scrollContainer.offsetWidth;
     }
-    console.log(scrollContainer.scrollLeft)
-    console.log(scrollContainer.scrollWidth)
+
+    // get left right icon
+    const leftIcon = document.querySelector('.left-btn');
+    const rightIcon = document.querySelector('.right-btn');
+    if (scrollContainer.scrollLeft <= 0) {
+        hideIcon(leftIcon);
+        showIcon(rightIcon);
+    } else if (scrollContainer.scrollLeft > 0 && scrollContainer.scrollLeft < scrollContainer.scrollWidth - scrollContainer.offsetWidth) {
+        showIcon(leftIcon);
+        showIcon(rightIcon);
+    } else if (scrollContainer.scrollLeft >= scrollContainer.scrollWidth - scrollContainer.offsetWidth) {
+        showIcon(leftIcon);
+        hideIcon(rightIcon);
+    }
+});
+
+scrollContainer.addEventListener("touchmove", (evt) => {
 
     // get left right icon
     const leftIcon = document.querySelector('.left-btn');
